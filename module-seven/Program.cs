e77723e97db87d95086f993284cc2a7d82b638ee
sahil-naik-kilowott <92841842+sahil-naik-kilowott@users.noreply.github.com>
2021-11-29 16:51:12 +0530

    Add project files.

---
+++ module-seven/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string orderID in orderIDs)
+{
+    if (orderID.StartsWith("B"))
+    {
+        Console.WriteLine(orderID);
+    }
+}
